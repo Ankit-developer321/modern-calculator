2a24a1fd88892086b3edab840cbed9f2132143f9
Ankit-developer321 <ankitbhatia09392@gmail.com>
2026-09-13 01:38:33 +0530

    Rename Android app to Calculator

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.modern.calculator',
-  appName: 'Modern Calculator',
+  appName: 'Calculator',
   webDir: 'public',
   bundledWebRuntime: false
 };
